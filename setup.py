0f5b73eee9e189dc950d2fda0a9217dded256712
David Miheev <me@davidkorol.life>
2023-01-25 23:46:56 +0300

    implement dataset class, small code improvements, small documentation improvements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name='GalaxyWitness',
-    version='0.2.2',
+    version='0.2.3',
     license='MIT',
     author='David Miheev',
     author_email='me@davidkorol.life',
