e8d430a29bf4327c8c09316264d2a1452e996d4b
David Miheev <me@davidkorol.life>
2023-04-17 11:51:10 +0000

    many small improvements, new experiments

--- galaxywitness/tests/test_betti.py
+++ galaxywitness/tests/test_betti.py
@@ -10,34 +10,34 @@ from galaxywitness.base_complex import BaseComplex
 from galaxywitness.alpha_complex import AlphaComplex
 from galaxywitness.datasets import Dataset
 
-dataset = Dataset('Galaxies_400K')
-dataset.download()
-path = os.path.abspath('./') +  '/data/' + 'Galaxies_400K.csv'
-df = pd.read_csv(path)
+def test_betti():
+    dataset = Dataset('Galaxies_400K')
+    dataset.download()
+    path = os.path.abspath('./') +  '/data/' + 'Galaxies_400K.csv'
+    df = pd.read_csv(path)
 
-n_gal = 5000
-type_of_complex = 'alpha'
-first_witness = 0
+    n_gal = 5000
+    first_witness = 0
 
-column_nums = [4, 5, 6]
-column_names = [list(df)[column_nums[0]], list(df)[column_nums[1]], list(df)[column_nums[2]]]
+    column_nums = [4, 5, 6]
+    column_names = [list(df)[column_nums[0]], list(df)[column_nums[1]], list(df)[column_nums[2]]]
 
-points = np.array(df[column_names].values[first_witness:n_gal + first_witness])
+    points = np.array(df[column_names].values[first_witness:n_gal + first_witness])
 
-coord = SkyCoord(
-    ra=points[:, 0] * u.degree,
-    dec=points[:, 1] * u.degree,
-    distance=Distance(z=points[:, 2])
-)
+    coord = SkyCoord(
+        ra=points[:, 0] * u.degree,
+        dec=points[:, 1] * u.degree,
+        distance=Distance(z=points[:, 2])
+    )
 
-points = np.transpose(np.array(coord.cartesian.xyz), (1, 0))
-landmarks = points
+    points = np.transpose(np.array(coord.cartesian.xyz), (1, 0))
+    landmarks = points
 
-complex_ = BaseComplex()
-complex_.__class__ = AlphaComplex
-complex_.__init__(points=landmarks)
+    complex_ = BaseComplex()
+    complex_.__class__ = AlphaComplex
+    complex_.__init__(points=landmarks)
 
-key_complex_type = 'gudhi'
-complex_.compute_simplicial_complex(d_max=3, r_max=7.5, custom=(key_complex_type == 'custom'))
+    key_complex_type = 'gudhi'
+    complex_.compute_simplicial_complex(d_max=3, r_max=7.5, custom=(key_complex_type == 'custom'))
 
-assert len(complex_.get_persistence_betti(dim=3, magnitudes=[7.5, 7.5 / 2.0, 7.5 / 2.0])) != 0
+    assert len(complex_.get_persistence_betti(dim=3, magnitudes=[7.5, 7.5 / 2.0, 7.5 / 2.0])) != 0
--- galaxywitness/tests/test_tomato.py
+++ galaxywitness/tests/test_tomato.py
@@ -12,39 +12,38 @@ from galaxywitness.base_complex import BaseComplex
 from galaxywitness.alpha_complex import AlphaComplex
 from galaxywitness.datasets import Dataset
 
+def test_tomato():
+    dataset = Dataset('Galaxies_400K')
+    
+    dataset.download()
+    path = os.path.abspath('./') +  '/data/' + 'Galaxies_400K.csv'
+    df = pd.read_csv(path)
 
-dataset = Dataset('Galaxies_400K')
-#print(dataset.inner_names)
-dataset.download()
-path = os.path.abspath('./') +  '/data/' + 'Galaxies_400K.csv'
-df = pd.read_csv(path)
+    n_gal = 5000
+    first_witness = 0
 
-n_gal = 5000
-type_of_complex = 'alpha'
-first_witness = 0
+    column_nums = [4, 5, 6]
+    column_names = [list(df)[column_nums[0]], list(df)[column_nums[1]], list(df)[column_nums[2]]]
 
-column_nums = [4, 5, 6]
-column_names = [list(df)[column_nums[0]], list(df)[column_nums[1]], list(df)[column_nums[2]]]
+    points = np.array(df[column_names].values[first_witness:n_gal + first_witness])
 
-points = np.array(df[column_names].values[first_witness:n_gal + first_witness])
+    coord = SkyCoord(
+        ra=points[:, 0] * u.degree,
+        dec=points[:, 1] * u.degree,
+        distance=Distance(z=points[:, 2])
+    )
 
-coord = SkyCoord(
-    ra=points[:, 0] * u.degree,
-    dec=points[:, 1] * u.degree,
-    distance=Distance(z=points[:, 2])
-)
+    points = np.transpose(np.array(coord.cartesian.xyz), (1, 0))
+    landmarks = points
 
-points = np.transpose(np.array(coord.cartesian.xyz), (1, 0))
-landmarks = points
+    complex_ = BaseComplex()
+    complex_.__class__ = AlphaComplex
+    complex_.__init__(points=landmarks)
 
-complex_ = BaseComplex()
-complex_.__class__ = AlphaComplex
-complex_.__init__(points=landmarks)
+    key_complex_type = 'custom'
+    complex_.compute_simplicial_complex(d_max=3, r_max=7.5, custom=(key_complex_type == 'custom'))
 
-key_complex_type = 'custom'
-complex_.compute_simplicial_complex(d_max=3, r_max=7.5, custom=(key_complex_type == 'custom'))
+    t = Tomato(density_type = 'logDTM', graph_type='knn')
+    t.fit(complex_.points)
 
-t = Tomato(density_type = 'logDTM', graph_type='knn')
-t.fit(complex_.points)
-
-assert t.n_clusters_ != 0
+    assert t.n_clusters_ != 0
